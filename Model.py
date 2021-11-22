4c312d8085bd817990fc2fe50119e695e5c3cb15
Tobias Christiansen <tobi@tobyase.de>
2021-11-22 17:55:30 +0100

    Add depth getter to model

--- Model.py
+++ Model.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 
 
@@ -5,12 +6,17 @@ class Model:
 
     def __init__(self) -> None:
         self.__layers = []
+        self.__boundaryDepths: List(float) = []
 
     def numberOfLayers(self) -> int:
         return len(self.__layers)
 
     def addLayer(self, height: float, speed: float) -> None:
         self.__layers.append(Layer(height, speed))
+        if len(self.__boundaryDepths) == 0:
+            self.__boundaryDepths.append(height)
+        else:
+            self.__boundaryDepths.append(self.__boundaryDepths[-1] + height)
 
     def layer(self, index: int) -> 'Layer':
         if not (0 <= index < self.numberOfLayers()):
@@ -21,6 +27,11 @@ class Model:
         if bottomLayer > self.numberOfLayers() - 1:
             return -1
         return np.arcsin(self.layer(bottomLayer - 1).speed / self.layer(bottomLayer).speed)
+
+    def depthAtBoundary(self, boundaryIndex):
+        if boundaryIndex > len(self.__boundaryDepths):
+            return -1
+        return self.__boundaryDepths[boundaryIndex]
 
 
 class Layer:
